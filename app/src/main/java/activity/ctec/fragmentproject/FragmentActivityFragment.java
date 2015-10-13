9a7154724c1394ef5ff88a84655d4e4a979bc206
Rinzler838 <dvil4688@740s-pl-105.local>
2015-10-13 08:13:17 -0600

    Created project to toy with fragments

---
+++ app/src/main/java/activity/ctec/fragmentproject/FragmentActivityFragment.java
@@ -0,0 +1,27 @@
+package activity.ctec.fragmentproject;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class FragmentActivityFragment extends Fragment
+{
+
+    public FragmentActivityFragment()
+    {
+        
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_, container, false);
+    }
+
+}
